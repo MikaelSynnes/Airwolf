95da76f1d8e7f25d5ddbdb80542c206901c95c7c
kristofferlie <kristoffer.g.lie@gmail.com>
2016-10-25 10:48:11 +0200

    oppdatert testsearch.php til knappen search

--- AirwolfGIT/public_html/Index.php
+++ AirwolfGIT/public_html/Index.php
@@ -63,8 +63,9 @@ while($myRow = odbc_fetch_array( $result )){
     </header>
     <body>
         <div class="row">
-		<form action="search.php?searching=true" method="POST">
-            <div class="col-1"><input type="text" name="searchTekst" value="" placeholder="Search.."/><input type="submit" value="Søk"/></div>
+
+<form action="testsearch.php" method="POST">
+            <div class="col-1"><input type="text" name="keyword" value="" placeholder="Search.."/><input type="submit" value="Søk"/></div>
             <div class="col-2"></div>
             <a href="handlekurv"><div class="col-3">Handlekurv</div></a>
         </div>
