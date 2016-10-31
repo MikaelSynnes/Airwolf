9b3c7f89b76aba5430fcb78a9417619ff5ca088e
Ole Martin T. Vad <omtvad@gmail.com>
2016-10-31 12:05:44 +0100

    Fikset noen flere linker

--- AirwolfGIT/public_html/Laptop.php
+++ AirwolfGIT/public_html/Laptop.php
@@ -67,12 +67,12 @@ while($myRow = odbc_fetch_array( $result )){
 <form action="testsearch.php" method="POST">
             <div class="col-1"><input type="text" name="keyword" value="" placeholder="Search.."/><input type="submit" value="Søk"/></div>
             <div class="col-2"></div>
-            <a href="handlekurv"><div class="col-3">Handlekurv</div></a>
+            <a href="handlekurv.php"><div class="col-3">Handlekurv</div></a>
         </div>
    
         <aside>
             <ul>
-                <li><a href="KatNr1">Laptop</a>
+                <li><a href="index.html">Laptop</a>
                 <li><a href="KatNr2">Stasjonær</a>
                <li><a href="KatNr3">Utstyr</a>
             </ul>
