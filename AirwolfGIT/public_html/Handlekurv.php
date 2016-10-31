2f645ace110704c3005f36eb670b4bdac96fda65
kristofferlie <kristoffer.g.lie@gmail.com>
2016-10-31 14:01:48 +0100

    Handlekurv ferdig skriptet.

--- AirwolfGIT/public_html/Handlekurv.php
+++ AirwolfGIT/public_html/Handlekurv.php
@@ -2,6 +2,7 @@
 session_start();
 if (isset($_SESSION['username'])) {
     echo "You are logged in as : {$_SESSION['username']}<p><a href='logout.php'>Logout</a></p>";
+echo "Ditt Ordrenr er : {$_SESSION['sesOrdre']}";
 }
 
 ?>
@@ -49,33 +50,56 @@ $connection_string = "DRIVER={SQL Server};SERVER=$server;$port;DATABASE=$databas
 $conn = odbc_connect($connection_string, $user, $pass);
 
 
-$ONr = isset($_SESSION['sesOrdre']);
+$ONr = $_SESSION['sesOrdre'];
 $showcart = ("SELECT VNr FROM Ordrelinje WHERE OrdreNr='$ONr'");
 $result = odbc_exec($conn, $showcart);
 $vare = array();
-while ($vare = odbc_fetch_array($result)) {
-    $vareInt = (int) $vare;
-    $result2 = odbc_exec($conn, ("SELECT * FROM Vare WHERE VNr='$vareInt'"));
+//echo "$vare";
+$p=3;
+//while ($vare = odbc_fetch_array($result)) {
+   // $vareInt = (int) $vare;
+   //$vare2= odbc_exec($conn, ("SELECT VNr FROM OrdreLinje WHERE OrdreNr='$ONr'"));
+    //$vare3= odbc_result($vare2,1);
+    $result2 = odbc_exec($conn, ("SELECT * FROM Vare WHERE VNr='$p'"));
+   // $result2 = "SELECT * FROM Vare WHERE VNr='$p'";
+    //$result3= odbc_exec($conn, ("SELECT VNr FROM OrdreLinje WHERE OrdreNr='$ONr'"));
     $result_arr = array();
     while ($result_arr = odbc_fetch_array($result2)) {
         ?> <article><?php
-            echo "<img src='images/" . $result_arr['Img'] . "' alt='laptop' width='30' height='40'>";
+          //PUTT EN IF SETNING SOM SJEKKER OM DU HAR NOE I ORDRELINJEN HER ELSE "DU HAR INGENTING I HANDLEKURV"
+           $vID=$result_arr['VNr'];
+            $test = "SELECT * FROM OrdreLinje Where OrdreNr='$ONr'";
+            $test1 = "SELECT VNr FROM OrdreLinje";
+            
+
+
+    $query = odbc_exec($conn, $test);
+    $array = odbc_fetch_array($query);
+    $verdi=stripslashes($array['OrdreNr']);
+
+    $query1 = odbc_exec($conn, $test1);
+    $array1= odbc_fetch_array($query1);
+    $verdi1=stripslashes($array['VNr']);
+
+
+             if($verdi == $ONr && $verdi1 == $vID){
+            echo "<img src='images/" . $result_arr['Img'] . "' alt='laptop' width='100' height='150'>";
             echo "   ";
             echo $result_arr['VareNavn'];
             echo "   ";
             echo $result_arr['Pris'];
-
-             $vID=$result_arr['VNr'];
-echo "<br>";
-             echo $ONr;
-echo "<br>";
-             echo $vID;
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-		
-		$removeItem=("DELETE FROM OrdreLinje WHERE OrdreNr='$ONr' AND VNr='$vID'");
+            }
+            else echo "Ingenting i handlekurv";
+            
+                if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $removeItem=("DELETE FROM OrdreLinje WHERE OrdreNr='$ONr' AND VNr='$vID'");
 		odbc_exec($conn, $removeItem);
 	}
+echo "<br>";
+            
+echo "<br>";
+     
+
 
 
             ?>
@@ -87,7 +111,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         
         echo "<br>";
     }
-}
+//}
 ?>       
 </body>
 </html>
