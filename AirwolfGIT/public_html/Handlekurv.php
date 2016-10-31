9b267e19d8e7ce53a41c200ed51904f522b7a9b4
kristofferlie <kristoffer.g.lie@gmail.com>
2016-10-31 13:07:41 +0100

    ++viser at du er logget inn

--- AirwolfGIT/public_html/Handlekurv.php
+++ AirwolfGIT/public_html/Handlekurv.php
@@ -1,110 +1,94 @@
 <?php
-//Use the machine name and instance if multiple instances are used
-$server = '158.38.101.83';
-$user = 'Synnes';
-$pass = '4307';
-//Define Port
-$port='Port=1433';
-$database = 'Airwolf';
-
-$connection_string = "DRIVER={SQL Server};SERVER=$server;$port;DATABASE=$database";
-$conn = odbc_connect($connection_string,$user,$pass);
-if ($conn) {
-    echo "Connection established.";
-} else{
-    die("Connection could not be established.");
+session_start();
+if (isset($_SESSION['username'])) {
+    echo "You are logged in as : {$_SESSION['username']}<p><a href='logout.php'>Logout</a></p>";
 }
 
-$sql = "SELECT KortBeskrivelse  FROM Vare" ;
-$print =odbc_exec($conn,$sql);
-
-$product_array = "SELECT *  FROM Vare" ;
-$result =odbc_exec($conn,$product_array);
-
-
-$rows = array();
-
-
-while($myRow = odbc_fetch_array( $result )){
-    $rows[] = $myRow;
-}
 ?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<script src="js/jquery.min.js"></script>
-<title>Group project datamodellering</title>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="Css.CSS">
-<header>
-    <div id="header">
-        Airwolf
+    <head>
+        <meta charset="UTF-8">
+        <title>Airwolf - Handlekurv</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="Css.CSS">
+    <header><div id="header"> Airwolf </div> 
+        <div id="login"> 
+            <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+            <form action="login.html" method="POST">
+                <input type= "submit" Value="login"/>
+            </form>
+            <form action="newuser.html" method="POST">
+                <input type= "submit" value="Registrer deg"/>
+            </form>
+        </div>       
+    </header>
+    <div class="row">
+        <div class="col-1"><input type="text" name="keyword" value="" placeholder="Search.."/><input type="submit" value="Søk"/></div>
+        <div class="col-2"></div>
+        <a href="handlekurv.html"><div class="col-3">Handlekurv</div></a>
     </div>
-    <div id="login">
-        <form action="link" method="POST">
-            <input type= "submit" Value="login"/>
-        </form>
-        <form action="newuser.html" method="POST">
-            <input type= "submit" value="Registrer deg"/>
-        </form>
-    </div>
-</header>
-<body>
-<div class="row">
-    <form action="search.php?searching=true" method="POST">
-        <div class="col-1">
-            <input type="text" name="searchTekst" value="" placeholder="Search.."/><input type="submit" value="Søk"/>
-        </div>
-    </form>
-    <div class="col-2"></div>
-    <a href="handlekurv"><div class="col-3">Handlekurv </div></a>
 </div>
 <aside>
     <ul>
         <li><a href="KatNr1"></a>
-        <li><a href="KatNr2">Test</a>
-        <li><a href="KatNr3">Test</a>
-        <li><a href="KatNr4">Test</a>
+        <li><a href="KatNr2">Laptop</a>
+        <li><a href="KatNr3">Stasjonær</a>
+        <li><a href="KatNr4">Utstyr</a>
     </ul>
 </aside>
-<div id="product">
-    <a href="VNr1.html">
-        <article>
-            <img src="images/<?php echo$rows[0]['Img'];?>" width="175" height="200" alt="Laptop"/>
-            <div id="Tekst">
-                <?php  echo $rows[0]['KortBeskrivelse'];  ?>
-            </div>
+<?php
+
+$server = '158.38.101.83';
+$user = 'Synnes';
+$pass = '4307';
+//Define Port
+$port = 'Port=1433';
+$database = 'Airwolf';
+$connection_string = "DRIVER={SQL Server};SERVER=$server;$port;DATABASE=$database";
+$conn = odbc_connect($connection_string, $user, $pass);
+
+
+$ONr = isset($_SESSION['sesOrdre']);
+$showcart = ("SELECT VNr FROM Ordrelinje WHERE OrdreNr='$ONr'");
+$result = odbc_exec($conn, $showcart);
+$vare = array();
+while ($vare = odbc_fetch_array($result)) {
+    $vareInt = (int) $vare;
+    $result2 = odbc_exec($conn, ("SELECT * FROM Vare WHERE VNr='$vareInt'"));
+    $result_arr = array();
+    while ($result_arr = odbc_fetch_array($result2)) {
+        ?> <article><?php
+            echo "<img src='images/" . $result_arr['Img'] . "' alt='laptop' width='30' height='40'>";
+            echo "   ";
+            echo $result_arr['VareNavn'];
+            echo "   ";
+            echo $result_arr['Pris'];
+
+             $vID=$result_arr['VNr'];
+echo "<br>";
+             echo $ONr;
+echo "<br>";
+             echo $vID;
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		$removeItem=("DELETE FROM OrdreLinje WHERE OrdreNr='$ONr' AND VNr='$vID'");
+		odbc_exec($conn, $removeItem);
+	}
+
+
+            ?>
+            <form method="POST" action="">
+                <input type="submit" value="Remove"></form></article>
         </article>
-    </a>
+        <?php
 
-    <?php  echo $rows[1]['Beskrivelse'];  ?>
-    <p>Quantity</p>
-    <select name="quantity">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-    </select>
-    <form action="">
-        <input type="submit" value="Kjøp" />
-    </form>
-
-</div>
-<br>
+        
+        echo "<br>";
+    }
+}
+?>       
 </body>
-<footer>
-    <div class="push"></div>
-    <div class="container">
-        <div class="footerFloater">
-            © 2016 - Airwolf SB
-        </div>
+</html>
 
-        <div class="footerFloater">
-            <a href="" class="btn"> Contact </a>
-            <a href="" class="btn">About</a>
-            <a href="" class="btn">FAQ</a>
-        </div>
-    </div>
-</footer>
-</html>|
